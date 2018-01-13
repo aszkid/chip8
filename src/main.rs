49aa474e8859a9289d4d2bf59e3a4d3e602215ed
aszkid <polgomezriquelme@gmail.com>
2018-01-13 02:04:43 -0600

    First commit

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+/**
+* http://mattmik.com/files/chip8/mastering/chip8.html
+* http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+* http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+*/
+
+const MEMORY_SIZE: usize = 4096;
+const NUM_REGISTERS: usize = 16;
+const STACK_SIZE: usize = 16;
+
+struct Chip {
+      memory: [u8; MEMORY_SIZE],
+      registers: [u8; NUM_REGISTERS],
+      running: bool,
+      stack: [u16; STACK_SIZE],
+      stack_pointer: u8,
+      program_counter: u16,
+      rom: String
+}
+
+impl Chip {
+      fn new() -> Chip {
+            Chip {
+                  memory: [0; MEMORY_SIZE],
+                  registers: [0; NUM_REGISTERS],
+                  running: false,
+                  stack: [0; STACK_SIZE],
+                  stack_pointer: 0,
+                  program_counter: 0x200,
+                  rom: String::from("")
+            }
+      }
+
+      fn set_flag(&mut self, val: u8) {
+            self.registers[0xF] = val;
+      }
+      fn get_flag(&self) -> u8 {
+            self.registers[0xF]
+      }
+
+      fn reset() {
+
+      }
+}
+
+fn main() {
+
+      let mut chip = Chip::new();
+      chip.set_flag(231);
+
+      println!("Program counter: {}", chip.program_counter);
+      println!("Flag: {}", chip.get_flag());
+      println!("ROM: {}", chip.rom);
+
+}
